1891befab32c04c8429b9b737369bf9ef7e8f2fe
yknomeh <yknomeh@protonmail.com>
2018-10-26 16:58:11 +0200

    Happy B-day

---
+++ src/LoveShell.cr
@@ -0,0 +1,26 @@
+require "fancyline"
+require "colorize"
+
+module LoveShell
+  VERSION = "0.1.0"
+  
+ prompt = "(LOVE): ".colorize(:red)
+ fancy = Fancyline.new
+
+ fancy.display.add do |ctx, line, yielder|
+    line = line.gsub(/^\w+/, &.colorize(:light_red).mode(:underline))
+    line = line.gsub(/(\|\s*)(\w+)/) do
+      "#{$1}#{$2.colorize(:light_red).mode(:underline)}"
+    end
+
+    line = line.gsub(/--?\w+/, &.colorize(:red))
+
+    yielder.call ctx, line
+  end
+
+  while input = fancy.readline(":")
+    break if input == "exit"
+
+    system(input)
+  end  
+end

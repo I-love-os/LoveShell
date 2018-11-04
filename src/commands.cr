2997c2edc0f661c79aa1df19f3de53ab9ccf1ca7
yknomeh <yknomeh@protonmail.com>
2018-11-04 21:55:28 +0100

    Command not found fix again

--- src/commands.cr
+++ src/commands.cr
@@ -35,7 +35,7 @@ class Commands
            cmd_exists = true
          end
       end
-      if !{'.', '/'}.includes? command
+      if (command.includes? '.') || (command.includes? '/')
         cmd_exists = true
       end
     else
